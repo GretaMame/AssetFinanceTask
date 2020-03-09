013c7cff6a0f2b01c1f5d88314ad9f995e7483dd
Greta Mameniskyte <gretuka27@gmail.com>
2020-03-10 00:39:15 +0200

    Add project files.

---
+++ AssetFinanceTask/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace AssetFinanceTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Consts.Input.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+            var input = arr.Select(int.Parse).ToArray();
+
+            PyramidTree Tree = new PyramidTree();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                Tree.AddNode(input[i]);
+            }            
+        }
+    }
+}
---
+++ AssetFinanceTask/PyramidTree.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace AssetFinanceTask
+{
+    [DebuggerDisplay("{Value}")]
+    public class Node
+    {
+        public int Value { get; set; }
+
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+        
+        public Node(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class PyramidTree
+    {
+        public Node Root { get; protected set; }
+
+        private Node lastAddedNode;
+
+        private int level;
+
+        private int nodesInLevel;
+
+        private Queue<Node> Queue = new Queue<Node>();
+
+        public PyramidTree()
+        {
+        }
+
+        public PyramidTree(int value)
+        {
+            Root = CreateNode(value);
+            level = 1;
+        }
+        
+        public void AddNode(int value)
+        {
+            if (nodesInLevel == level)
+            {
+                level++;
+                nodesInLevel = 0;
+            }
+
+            if (Root == null)
+            {
+                Root = CreateNode(value);               
+            }
+            else
+            {
+                var node = Queue.Peek();
+                if (node.Left == null)
+                {
+                    if (nodesInLevel == 0)
+                    {
+                        node.Left = CreateNode(value);
+                    }
+                    else
+                    {
+                        node.Left = lastAddedNode;
+                        node.Right = CreateNode(value);
+                        Queue.Dequeue();
+                    }
+                }
+                else if (node.Right == null)
+                {
+                    node.Right = CreateNode(value);
+                    Queue.Dequeue();
+                }
+            }
+        }
+
+        private Node CreateNode(int value)
+        {
+            var node = new Node(value);
+            Queue.Enqueue(node);
+            lastAddedNode = node;
+            nodesInLevel++;
+
+            return node;
+        }
+    }
+}
